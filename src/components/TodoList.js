456e962901f78f734669ea709db27d56a4486533
loshjarson <jlarson129@outlook.com>
2021-08-04 20:33:53 -0400

    created TodoList component and state value

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = props => {
+    
+    return (
+      <div className="todo-list">
+        {props.Todos.map(todo => (
+          <Todo key={todo.id} todo={todo}/>
+        ))}
+        <button onClick={props.clearAll} className="clear-btn">
+          Clear All
+        </button>
+      </div>
+    );
+  };
+
+export default GroceryList;
